788566e5630bb83e40a89bbc78b3e14bc9c40610
Ahmed Raihan <imraihan00@gmail.com>
2023-11-07 13:45:36 +0600

    remove unnecessary code

--- client/src/features/userslice.js
+++ client/src/features/userslice.js
@@ -7,48 +7,37 @@ const initialState = {
 
 
 export const registerUser = createAsyncThunk('user/register', async (userData, thunkAPI) => {
-  try {
-    const response = await fetch('http://localhost:3001/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
+  const response = await fetch('http://localhost:3001/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 
-    if (!response.ok) {
-      const data = await response.json();
-      throw new Error(data.message);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
+  if (!response.ok) {
+    throw new Error('Failed to register');
   }
+
+  return await response.json();
 });
 
 export const loginUser = createAsyncThunk('user/login', async (userData, thunkAPI) => {
-  try {
-    const response = await fetch('http://localhost:3001/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData),
-    });
+  const response = await fetch('http://localhost:3001/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 
-    if (!response.ok) {
-      const data = await response.json();
-      throw new Error(data.message);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
+  if (!response.ok) {
+    throw new Error('Failed to log in');
   }
+
+  return await response.json();
 });
+
 
 const userSlice = createSlice({
   name: 'user',
